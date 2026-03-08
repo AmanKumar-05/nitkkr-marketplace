ac84f463382721884372474343f5036434ea0ed1
Aman Kumar <aman@Amans-MacBook-Air.local>
2026-03-08 23:22:13 +0530

    switch email service to resend

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,18 +1,10 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, text) {
-  await transporter.sendMail({
-    from: `"NIT KKR Marketplace" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "NIT KKR Marketplace <onboarding@resend.dev>",
     to: to,
     subject: subject,
     text: text
