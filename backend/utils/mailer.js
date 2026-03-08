64e2e60cbb9c5e4057076712516afbb1647108fe
Aman Kumar <aman@Amans-MacBook-Air.local>
2026-03-08 23:02:05 +0530

    fix gmail smtp port

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,22 +1,25 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // important
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
 async function sendEmail(to, subject, text) {
-
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: to,
     subject: subject,
     text: text
   });
-
 }
 
 module.exports = sendEmail;
