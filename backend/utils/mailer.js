abdc8e050105435edb3fbb744b381f1905d35f1b
Aman Kumar <aman@Amans-MacBook-Air.local>
2026-03-09 22:01:05 +0530

    use verified resend domain

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -4,11 +4,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, text) {
   await resend.emails.send({
-    from: "NIT KKR Marketplace <onboarding@resend.dev>",
-    to: to,
-    subject: subject,
-    text: text
-  });
+  from: "NIT KKR Marketplace <otp@nitkkrmarketplace.me>",
+  to: to,
+  subject: subject,
+  text: text
+});
 }
 
 module.exports = sendEmail;
